ef740a7d5d3b0ab224cc1345a2679c0b0c1b0790
gravitybrands <derek@gravitybrands.com>
2021-04-01 21:58:53 -0700

    Update create.ts

--- src/create.ts
+++ src/create.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express'
 
 export type Create<I extends string | number, R> = (
-  body: R
+  body: R, req: R
 ) => Promise<R & { id: I }>
 
 export const create = <I extends string | number, R>(
